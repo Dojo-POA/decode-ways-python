b2625dd688dc90db738170ef56b6c833ed6743f4
Bruno Tavares <bruno.exz@gmail.com>
2014-01-22 20:32:32 -0200

    Aalll green

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+offset = 96
+
+def chr_from_int(n):
+  return chr(n+offset) if n != 0 else ''
+
 def decode(something):
- if something <= 10:
-  return 'j' if something == 10 else 'a'
- else:
-  return ['aa', 'k']
+  if something <= 10:
+    return [chr_from_int(something)]
+  else:
+    result = ["".join([chr_from_int(int(i)) for i in str(something)])]
+
+    if something < 27: 
+      result.append(chr_from_int(something))
+
+    return result
--- test/main_test.py
+++ test/main_test.py
@@ -1,10 +1,21 @@
 from main import *
+from nose.tools import assert_equals
 
-class TestGenerationOfPosibilities:
-  def test_single_element(self):
-    assert decode(1) == 'a'
+class TestGenerationOfPossibilities:
+  def test_single_digit(self):
+    assert_equals(decode(1), ['a'])
+    assert_equals(decode(2), ['b'])
+    assert_equals(decode(3), ['c'])
+    assert_equals(decode(4), ['d'])
+    assert_equals(decode(5), ['e'])
+    assert_equals(decode(6), ['f'])
+    assert_equals(decode(7), ['g'])
+    assert_equals(decode(8), ['h'])
+    assert_equals(decode(9), ['i'])
 
-  def test_two_elements(self):
-    assert decode(10) == 'j'
-    assert decode(11) == ['aa', 'k']
-    
+  def test_two_digits(self):
+    assert_equals(decode(10), ['j'])
+    assert_equals(decode(11), ['aa', 'k'])
+
+  def test_five_digits(self):
+    assert_equals(decode(88888), ['hhhhh'])
